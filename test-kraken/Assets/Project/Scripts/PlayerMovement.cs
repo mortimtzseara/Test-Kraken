14fc52ba6cddc46e6f13d46214c90697423c093c
Morti <morti.martinez-seara.7e9@itb.cat>
2026-09-25 14:19:42 +0200

    feat: Exercise 6 - RayCast

--- test-kraken/Assets/Project/Scripts/PlayerMovement.cs
+++ test-kraken/Assets/Project/Scripts/PlayerMovement.cs
@@ -5,20 +5,30 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 7f;
     private Rigidbody2D rb;
+    public float rayLength = 0.6f;
+    public LayerMask groundLayer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
+	}
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal"); // -1 or 1
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
+        Debug.DrawRay(transform.position, Vector2.down*rayLength, Color.red);
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, groundLayer);
+
+        return hit.collider != null;
     }
 }
